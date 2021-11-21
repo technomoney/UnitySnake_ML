f89ae41f09c7d7b02cc1f32f02d35e43621ed522
Dave O <daveoravec@gmail.com>
2021-11-21 12:23:16 -0500

    Adjusting Tail
    Working to get the tail to move to the end of the snake when new parts are added

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
@@ -41,8 +42,29 @@ public class Snake : MonoBehaviour
     {
         //make the cube and parent it under the main snake object
         var chunk = Instantiate(pfb_bodyChunk, transform);
+        m_list_bodyParts.Add(chunk);
         
         //plop the new part right behind the head
-        chunk.transform.localPosition = new Vector3(0, GameManager.YPosition, m_list_bodyParts.Count - 1);
+        chunk.transform.localPosition = new Vector3(0, 0, (m_list_bodyParts.Count - 1) * -1);
+        
+        //let's keep the tail at the back of the list
+        MoveTailToEndOfList();
+        
+        //we'll need to move the tail to stay behind everything else
+        var tail = m_list_bodyParts.Last();
+        tail.transform.localPosition = new Vector3(0, 0, (m_list_bodyParts.Count+1) * -1);
+
+    }
+
+    /// <summary>
+    /// Swaps the last and second to last positions in the body parts list, should be called right after adding a new
+    /// body part, depends on the tail being second to last...
+    /// </summary>
+    private void MoveTailToEndOfList()
+    {
+        var newBodyChunk = m_list_bodyParts.Last();
+        var listSize = m_list_bodyParts.Count;
+        m_list_bodyParts[listSize - 2] = m_list_bodyParts[listSize - 1];
+        m_list_bodyParts[listSize - 2] = newBodyChunk;
     }
 }
