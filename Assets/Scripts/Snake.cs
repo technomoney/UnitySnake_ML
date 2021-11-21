1bf2729c84fd75ad1bd1061d6549f2f95503deec
Dave O <daveoravec@gmail.com>
2021-11-21 14:10:43 -0500

    movement cont.
    Unity crash #2, not too bad...
    removed the lerp movement and now we just snap.  doesn't look as good per se, buit it does look move like snake.. this fixes our moving off the edge problem
    made the snake much faster for easier testing

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -33,15 +33,10 @@ public class Snake : MonoBehaviour
     public bool m_growBiggerAfterMovement = false;
 
     /// <summary>
-    /// control how fast the snake moves
-    /// </summary>
-    private float m_moveSpeed = 10f;
-
-    /// <summary>
     /// we can pause slightly between each movement
     /// </summary>
     private bool m_isPaused = false;
-    private float m_pauseTime = .07f;
+    private float m_pauseTime = .15f;
     private float m_pauseProgress = 0;
     
     void Start()
@@ -100,7 +95,8 @@ public class Snake : MonoBehaviour
     /// </summary>
     private void ContinueMoving()
     {
-        StartCoroutine(HandleMovement());
+        //StartCoroutine(HandleMovement());
+        HandleMovement();
     }
 
     /// <summary>
@@ -152,14 +148,8 @@ public class Snake : MonoBehaviour
         ContinueMoving();
     }
 
-    /// <summary>
-    /// co routine to handle moving the head and then getting every body part to follow
-    /// </summary>
-    IEnumerator HandleMovement()
+    private void HandleMovement()
     {
-        //get the position we want to move to next
-        //we'll have to track this per body chunk to get the classic snake-game movement style, so for now
-        //just get the target for the head and we'll loop through the rest
         var targetPositions = new List<Vector3>
         {
             new Vector3(
@@ -173,37 +163,6 @@ public class Snake : MonoBehaviour
             //in the loop (keeping the loop in order i.e. swapping the tail is key here)
             targetPositions.Add(m_list_bodyParts[x-1].localPosition);
         }
-        
-        //set our initial positions for the lerp
-        var initialPositions = new List<Vector3>();
-        //we can just do a foreach here since the process is the same for all parts including the head here
-        foreach (var part in m_list_bodyParts) initialPositions.Add(part.localPosition);
-        //lerp progress
-        var progress = 0f;
-
-        while (progress < 1)
-        {
-            //adjust the positions of the parts
-            for (int x = 0; x < m_list_bodyParts.Count; x++)
-                m_list_bodyParts[x].localPosition = Vector3.Lerp(initialPositions[x], targetPositions[x], progress);
-            
-            //increment the progress
-            progress += Time.deltaTime*m_moveSpeed;
-            //return from the coroutine until we're finished moving
-            yield return 0;
-        }
-        
-        //done moving
-        //when we're actually done moving start a pause
-        m_isPaused = true;
-        
-        //see if we ate something and need to grow
-        if (m_growBiggerAfterMovement)
-        {
-            AddBodyChunk();
-            m_growBiggerAfterMovement = false; //reset the flag
-        }
-        
         //we should also rectify the positions of the parts by manually setting them to their targets
         //this will remove andy weird rounding/lerp errors
         //we use the conditional check in the count to check against the list sizes of body parts and target positions
@@ -220,10 +179,19 @@ public class Snake : MonoBehaviour
             if (part.localPosition.x > 40) part.localPosition = new Vector3(-40, 0, 0);
             if (part.localPosition.z < -40) part.localPosition = new Vector3(0, 0, 40);
             if (part.localPosition.z > 40) part.localPosition = new Vector3(0,0, -40);
-
         }
-
-
+        
+        //done moving
+        //when we're actually done moving start a pause
+        m_isPaused = true;
+        
+        //see if we ate something and need to grow
+        if (m_growBiggerAfterMovement)
+        {
+            AddBodyChunk();
+            m_growBiggerAfterMovement = false; //reset the flag
+        }
+        
         //lets also fix the rotation of the tail
         m_list_bodyParts.Last().transform.rotation = Quaternion.Euler(0, m_movementBearing * -Mathf.Rad2Deg, 0);
     }
