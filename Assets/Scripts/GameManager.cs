75363382b686f116ad9787d95fb0b16910ec0179
Dave O <daveoravec@gmail.com>
2021-11-21 15:36:39 -0500

    final comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,20 +9,26 @@ public class GameManager : MonoBehaviour
 {
     public Snake snake;
     public int Score;
+    
+    //not a pure singleton, but a self referring static instance to the gamemanger,
+    //even though we only call this once, I like this pattern for gm-type 
+    //logic
     public static GameManager Inst;
 
+    //text fields for our basic info
     public TextMeshProUGUI m_text_score, m_text_banner;
 
     void Start()
     {
+        //set the reference 
         Inst = this;
         m_text_score.text = string.Empty;
         Score = 0;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        //probably don't really need to update every frame,... but no bog deal
         m_text_score.text = "Score: " + Score;
 
         //we could lock this behind the gameover screen, but this is handy for testing...
--- Assets/Scripts/HeadCollider.cs
+++ Assets/Scripts/HeadCollider.cs
@@ -11,7 +11,12 @@ public class HeadCollider : MonoBehaviour
     //we'll need to move the food after we eat it...
     public Food m_food;
     
-    
+    /// <summary>
+    /// we use trigger here instead of actual collision because we're moving the snake manually, not under the physics engine 
+    /// further, it's easier to use trigger since we're just plopping each body part where it's supposed to go.
+    /// Only the head and the food have rigidbodies and they're both kinetic, which is all we need to 
+    /// get the triggers to go off
+    /// </summary>
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Food"))
